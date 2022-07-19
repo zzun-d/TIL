8a2f8aa9921a4e05d2d1844d59e26d72db538e0b
zzun-d <dlwnsgur0803@gmail.com>
2022-07-19 17:48:01 +0900

    algo update

---
+++ Algorithm/codeup/6078.py
@@ -0,0 +1,5 @@
+while True:
+    s = input()
+    print(s)
+    if s == 'q':
+        break
---
+++ Algorithm/codeup/6079.py
@@ -0,0 +1,8 @@
+n = int(input())
+num = 1
+num_s = 1
+while num_s < n:
+    num += 1
+    num_s += num 
+    
+print(num)
