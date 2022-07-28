46eb03e4dfbb35183169f04e6d866519c7ccf34f
zzun-d <dlwnsgur0803@gmail.com>
2022-07-28 23:11:00 +0900

    algo
    softeer GBC.. 1 case fail ToT

---
+++ Algorithm/softeer/GBC(1case fail).py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+
+speed_limit = []
+for _ in range(n):
+    d, l_s = map(int, input().split())
+    speed_limit.extend([l_s] * d)
+
+test_speed = []
+for _ in range(m):
+    d, l_s = map(int, input().split())
+    test_speed.extend([l_s] * d)
+
+compare_speed = []
+for i in range(100):
+    compare_speed.append(test_speed[i] - speed_limit[i])
+
+print(max(compare_speed))
