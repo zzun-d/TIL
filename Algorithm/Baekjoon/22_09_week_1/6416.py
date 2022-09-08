9cb64a4a4d3d70aa403395217272d51ef436c032
zzun-d <dlwnsgur0803@gmail.com>
2022-09-08 15:53:18 +0900

    algo

--- Algorithm/Baekjoon/22_09_week_1/6416.py
+++ Algorithm/Baekjoon/22_09_week_1/6416.py
@@ -2,12 +2,12 @@ import sys
 from collections import defaultdict
 def input():
     return sys.stdin.readline().rstrip()
-
-
+    
 tc = 1
 while True:
     tree = defaultdict(list)
     TMP = True
+
     while True:
         n_lst = list(map(int, input().split()))
         for i in range(0, len(n_lst), 2):
@@ -17,20 +17,22 @@ while True:
         if tree[-1]:
             TMP = False
             break
+
     if TMP:
-        if len(tree) == 1:
+
+        if len(tree) <= 2:
             print(f'Case {tc} is a tree.')
+            tc += 1
             continue
 
         v_lst = []
         for i in list(tree.values()):
             v_lst += i
-        # print(v_lst)
+
         v_set = set(v_lst)
         k_set = set(tree.keys()) - set([-1])
         tmp = True
-        # print(k_set)
-        # print(v_set)
+
         if len(k_set - v_set) != 1 or len(v_lst) != len(v_set):
             tmp = False
         if tmp:
