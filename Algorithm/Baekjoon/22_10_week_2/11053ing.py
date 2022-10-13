66d661c5dc0cbb774dc499ddc2ec7b8d3aed0293
이유영 <dldbdud112@naver.com>
2022-10-13 18:01:47 +0900

    algo

--- Algorithm/Baekjoon/22_10_week_2/11053ing.py
+++ Algorithm/Baekjoon/22_10_week_2/11053ing.py
@@ -1,6 +1,10 @@
 N = int(input())
 lst = list(map(int, input().split()))
 dp = [0]*N
-
-for i in range(N):
-    pass
+dp[0] = 1
+for i in range(1, N):
+    for j in range(0, i):
+        if lst[i] > lst[j] and dp[j] > dp[i]:
+            dp[i] = dp[j]
+    dp[i] += 1
+print(max(dp))
