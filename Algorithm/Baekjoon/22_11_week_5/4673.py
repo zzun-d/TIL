57c90dfb965877e7e63254773fe72cfbda2259f5
zzun-d <dlwnsgur0803@gmail.com>
2022-11-30 19:54:35 +0900

    algo

--- Algorithm/Baekjoon/22_11_week_5/4673.py
+++ Algorithm/Baekjoon/22_11_week_5/4673.py
@@ -1,16 +1,20 @@
-nums = set(list(range(1, 101)))
+nums = set(list(range(1, 10001)))
 def d(n):
     global nums
+
     if nums.intersection({n}):
         
         nxt_n = n
         while n:
             nxt_n += n%10
             n = n//10
-        nums -= {nxt_n}
-        if nxt_n <= 100:
+        if nxt_n <= 10000:
             d(nxt_n)
+            nums -= {nxt_n}
     return
-for i in range(1, 101):
+
+for i in range(1, 10001):
     d(i)
-print(len(nums))
+nums = sorted(list(nums))
+for num in nums:
+    print(num)
