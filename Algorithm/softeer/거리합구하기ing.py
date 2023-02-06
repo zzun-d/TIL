4f5be8597a3a60a683dc742023d912ee1cbeb70f
이준혁 <dlwnsgur0803@gmail.com>
2023-02-07 08:55:08 +0900

    algo

--- Algorithm/softeer/거리합구하기ing.py
+++ Algorithm/softeer/거리합구하기ing.py
@@ -1,37 +1,37 @@
 from collections import defaultdict
+import heapq
 import sys
 
 def input():
     return sys.stdin.readline().rstrip()
 
-def D(s, cnt):
-    global ans
-
-    visited[s] = 1
-
-    for e, t in adj[s]:
-    
-        if visited[e]:
-            continue
-    
-        cntDict[e] = [D(e, cnt), t]
-        cnt += 1
-
-    return cnt
-
-
 N = int(input())
+INF = 2e11
 
-adj = defaultdict(list)
+distance = [INF]*(N+1)
+graph = [[] for _ in range(N+1)]
 
 for _ in range(N-1):
     x, y, t = map(int, input().split())
-    adj[x].append((y, t))
-    adj[y].append((x, t))
+    graph[x].append((y, t))
+
+
+q = []
+heapq.heappush(q, (0, 1))
+distance[1] = 0
+while q:
+    dist, now = heapq.heappop(q)
+    if distance[now] < dist:
+        continue
+    for i in graph[now]:
+        cost = dist + i[1]
+        if cost < distance[i[0]]:
+            distance[i[0]] = cost 
+            heapq.heappush(q, (cost, i[0]))
 
 for i in range(1, N+1):
-    cntDict = defaultdict(list)
-    visited = [0]*(N+1)
     ans = 0
-    D(i, 1)
-    print(cntDict)
+    for j in range(1, N+1):
+        ans += distance[i] + distance[j]
+    print(ans)
+
